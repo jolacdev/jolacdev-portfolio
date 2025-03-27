2f4de6ab31ab61edb5382c61fe882fe15e664b38
Jose L <58451072+jolacdev@users.noreply.github.com>
2025-03-27 16:22:59 +0100

    feat: :sparkles: create Heading component

---
+++ src/components/atoms/Heading.tsx
@@ -0,0 +1,29 @@
+import { HTMLProps, ReactNode } from 'react';
+
+type HeadingProps = {
+  children: ReactNode;
+  type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+} & HTMLProps<HTMLHeadingElement>;
+
+const Heading = ({ children, className = '', type, ...rest }: HeadingProps) => {
+  const Tag = type;
+
+  const headingStyles = {
+    h1: 'text-5xl font-extrabold mt-8 mb-8',
+    h2: 'text-3xl font-bold mt-8 mb-6',
+    h3: 'text-2xl font-semibold mt-6 mb-3',
+    h4: 'text-xl font-semibold mt-6 mb-3',
+    h5: 'text-lg font-medium mt-4 mb-2',
+    h6: 'text-base font-medium mt-4 mb-2',
+  };
+
+  const classNames = headingStyles[type];
+
+  return (
+    <Tag className={`${classNames} font-mono ${className}`} {...rest}>
+      {children}
+    </Tag>
+  );
+};
+
+export default Heading;
