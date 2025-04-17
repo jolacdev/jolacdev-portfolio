260e56f39353603c5d66d8c586b12525e8e6f6e1
Jose L <58451072+jolacdev@users.noreply.github.com>
2025-04-17 10:16:54 +0200

    fix props

--- src/components/molecules/AnchorHeading.tsx
+++ src/components/molecules/AnchorHeading.tsx
@@ -7,8 +7,18 @@ type AnchorHeadingProps = HeadingProps & {
   hash: string;
 };
 
-const AnchorHeading = ({ children, hash }: AnchorHeadingProps) => (
-  <Heading className="group relative flex items-center" id={hash} type="h2">
+const AnchorHeading = ({
+  children,
+  hash,
+  type,
+  ...rest
+}: AnchorHeadingProps) => (
+  <Heading
+    className="group relative flex items-center"
+    id={hash}
+    type={type}
+    {...rest}
+  >
     {children}
     <Link
       className="hover:text-coral-500 absolute -left-1 -translate-x-full cursor-pointer opacity-0 group-hover:opacity-100"
