f8a536e68667f7f39068692b8f9c934f24926b8b
Jose L <58451072+jolacdev@users.noreply.github.com>
2025-05-09 09:04:13 +0200

    chore: add prettier-plugin-tailwindcss for sorting Tailwind classes

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,6 +3,8 @@ export default {
   bracketSameLine: false,
   endOfLine: 'auto',
   jsxSingleQuote: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+  pluginSearchDirs: false,
   printWidth: 80,
   semi: true,
   singleQuote: true,
